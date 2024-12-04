7a0fb1ec9c1de342fa1c381f32d47a11deb36183
Richu Sony <sonyrichu4@gmail.com>
2024-12-04 12:38:39 +0530

    update: adding user metadata to clerk

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
 import { createUser } from '@/lib/actions/user.action'
 import { NextResponse } from 'next/server'
 
@@ -63,13 +63,13 @@ export async function POST(req: Request) {
     }
 
     const newUser = await createUser(user);
-    // if(newUser) {
-    //     (await clerkClient()).users.updateUserMetadata(id,{
-    //         publicMetadata: {
-    //             userId: newUser.id
-    //         }
-    //     })
-    // }
+    if(newUser) {
+        (await clerkClient()).users.updateUserMetadata(id,{
+            publicMetadata: {
+                userId: newUser.id
+            }
+        })
+    }
 
     return NextResponse.json({message: "New User Created", user: newUser});
   }
