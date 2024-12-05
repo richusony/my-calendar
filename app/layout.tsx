20aa7836588b19e9f87f0586ab272ad41d3f7f93
Richu Sony <sonyrichu4@gmail.com>
2024-12-05 09:28:43 +0530

    update: added signup and signin func

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,11 @@
-import type { Metadata } from "next";
-// import localFont from "next/font/local";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import type { Metadata } from "next";
+import { ClerkProvider, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "My Calender",
-  description: "Created for scheduling tasks",
+  description: "Created for scheduling tasks"
 };
 
 export default function RootLayout({
@@ -26,15 +15,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className=''>
-        <nav className="p-4">
-          <h1 className="ml-4 mt-2 border-2 border-gray-400 w-fit p-2 font-semibold text-xl text-gray-600 rounded-md shadow-md">My Calendar</h1>
-        </nav>
-        {children}
-        <footer className="p-5 text-center"><h3>Copyright© Richu Sony</h3></footer>
-      </body>
-    </html>
+      <html lang="en">
+        <body className=''>
+          <nav className="p-4 flex justify-between items-center">
+            <Link href="/" className="ml-4 mt-2 border-2 border-gray-400 w-fit p-2 font-semibold text-xl text-gray-600 rounded-md shadow-md">My Calendar</Link>
+            <SignedOut>
+              <Link href="/signin" className="bg-gray-800 w-fit py-1 px-4 text-center text-gray-100 font-semibold text-xl rounded-md shadow-md">Login</Link>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </nav>
+          {children}
+          <footer className="p-5 text-center"><h3>Copyright© Richu Sony</h3></footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
